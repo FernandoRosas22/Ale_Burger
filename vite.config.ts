5883a47ede26145e2e04991ce0149080641af133
FernandoRosas22 <fernandosebastianrosas1997@gmail.com>
2026-05-07 21:43:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
+  base: "/Ale_Burger/",
+
   plugins: [react()],
+
   resolve: {
-    alias: { "@": path.resolve(__dirname, "src") },
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
