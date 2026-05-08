d9127e98b947f353e9a69277344a09222c4217aa
Fernando Rosas <fernandosebastianrosas1997@gmail.com>
2026-05-07 22:39:04 -0300

    Update contact.ts

--- src/utils/contact.ts
+++ src/utils/contact.ts
@@ -1,9 +1,10 @@
-export const WHATSAPP_NUMBER = "0111521591833";
+export const WHATSAPP_NUMBER = "5491172106298";
 
 export const PEDIDOS_URL =
   "https://pedidos.masdelivery.com/aleburgers?utm_source=ig&utm_medium=social&utm_content=link_in_bio";
 
-export const INSTAGRAM_URL = "https://www.instagram.com/aleburgers.ok";
+export const INSTAGRAM_URL =
+  "https://www.instagram.com/aleburgers.ok";
 
 export const MAPS_EMBED_URL =
   "https://www.google.com/maps?q=-34.7205088,-58.7947361&hl=es&z=17&output=embed";
