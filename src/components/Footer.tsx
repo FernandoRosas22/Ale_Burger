e2f0ebad9aaac0635e7aa3f88279890366e6d234
Fernando Rosas <fernandosebastianrosas1997@gmail.com>
2026-05-18 00:18:22 -0300

    Update footer text and credit design changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <img src={logo} alt="AleBurgers" />
         <span>ALEBURGERS</span>
       </div>
-      <p>© {new Date().getFullYear()} AleBurgers. Hecho con 🍔 en Buenos Aires.</p>
+      <p>© {new Date().getFullYear()} Diseño y desarrollo web por @fer.rosas22 .</p>
       <p>
         Seguinos en{" "}
         <a href={INSTAGRAM_URL} target="_blank" rel="noreferrer">@aleburgers.ok</a>
